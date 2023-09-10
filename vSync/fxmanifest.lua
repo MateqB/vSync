f72c7cb0f4334e76cd308f8a557b5a6cf9c609ab
Mateq <mateqjs@gmail.com>
2023-09-10 20:48:39 +0200

    Move to fxmanifest

---
+++ vSync/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version  "cerulean"
+lua54       "yes"
+games       { "gta5" }
+version     "1.0.0"
+author      "Vespura"
+
+client_scripts {
+    'vs_client.lua',
+}
+
+server_scripts {
+    'vs_server.lua',
+}
